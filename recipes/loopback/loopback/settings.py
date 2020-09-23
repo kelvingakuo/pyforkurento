c067bdcbc3e375e3a6e1717b77d630364de37047
kelvingakuo <kelvingakuo@gmail.com>
2020-09-23 23:48:02 +0300

    Security fix

--- recipes/loopback/loopback/settings.py
+++ recipes/loopback/loopback/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from loopback.sec_settings import SEC
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bde$=(9k*=95tlr$tkk5=8ekm0ysjfovwx(i(3=9@oy-lgpj6j'
+SECRET_KEY = SEC
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
